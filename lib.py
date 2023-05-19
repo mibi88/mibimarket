8c0a255dcd0a946e895f588b94463a16d1b1d833
mibi88 <76903855+mibi88@users.noreply.github.com>
2023-05-19 16:35:36 +0200

    Added market and some other functions

--- lib.py
+++ lib.py
@@ -5,7 +5,9 @@ import random
 json_data = {}
 
 existing_items = ["gray fish", "worm", "box of sand", "golden tux",
-"plastic tux", "shovel", "fishing pole"]
+"plastic tux", "shovel", "fishing pole", "seaweed", "wetsuit", "marble tux",
+"golden treasure", "exotic fish", "multicolor fish", "luminescent fish",
+"golden fish", "diamond fish", "bank note"]
 
 DATABASE = "db.json"
 
@@ -31,10 +33,14 @@ if os.path.exists(DATABASE):
     load_db()
 else:
     json_data["users"] = {}
-    json_data["market"] = []
+    json_data["market"] = {}
+    json_data["marketprice"] = 3
     json_data["shop"] = {}
     json_data["prices"] = {"gray fish": 20, "box of sand": 40,
-    "golden tux": 320, "plastic tux": 6, "shovel": 25, "fishing pole": 65}
+    "golden tux": 320, "plastic tux": 6, "shovel": 25, "fishing pole": 65,
+    "wetsuit": 65, "marble tux": 220, "golden treasure": 860, "exotic fish": 25,
+    "multicolor fish": 45, "luminescent fish": 140, "golden fish": 280}
+    json_data["non_sellable_prices"] = {"worm": 1, "seaweed": 3}
     json_data["inflation"] = 0
     save_db()
 
@@ -47,9 +53,13 @@ def create_user(user):
     json_data["users"][user]["bank_max"] = 30000
     json_data["users"][user]["inventory"] = {}
     json_data["users"][user]["probas"] = {}
-    json_data["users"][user]["probas"]["fish"] = {"gray fish": 30}
+    json_data["users"][user]["probas"]["fish"] = {"gray fish": 30,
+    "exotic fish": 10, "multicolor fish": 5, "luminescent fish": 3,
+    "golden fish": 2, "diamond fish": 1}
     json_data["users"][user]["probas"]["dig"] = {"worm": 30, "box of sand": 10,
-    "golden tux": 1, "plastic tux": 6, "shovel": 2}
+    "golden tux": 1, "plastic tux": 6, "shovel": 2, "bank note": 4}
+    json_data["users"][user]["probas"]["dive"] = {"seaweed": 30,
+    "fishing pole": 10, "wetsuit": 5, "marble tux": 4, "golden treasure": 1}
 
 def get_profile(user):
     if not user in json_data["users"]:
@@ -64,9 +74,11 @@ def get_profile(user):
         newinventory += f"- {key} x{values}\n"
     if newinventory == "":
         newinventory = "You have nothing in your inventory."
-    return f"""##### COINS #####
-- ${wallet} in your wallet
-- ${bank}/${bank_max} in your bank account
+    return f"""##### PROFILE #####
+User {user}
+##### COINS #####
+- ${wallet} in the wallet
+- ${bank}/${bank_max} in the bank account
 ##### INVENTORY #####
 {newinventory}"""
 
@@ -99,6 +111,32 @@ def get_money(user, num):
 def has_money(user, num):
     if json_data["users"][user]["wallet"] >= int(num): return 1
     return 0
+
+def do_deposit(user, num):
+    if not user in json_data["users"]:
+        create_user(user)
+        save_db()
+    num = int(num)
+    if not has_money(user, num):
+        return f"You do not have ${num} in your wallet."
+    if json_data["users"][user]["bank"] + num <= json_data["users"][user]["bank_max"]:
+        json_data["users"][user]["bank"] += num
+        pay(user, num)
+        save_db()
+        return f"You deposited ${num} on your bank account."
+    else:
+        return "You need more space on your bank account."
+
+def do_withdraw(user, num):
+    if not user in json_data["users"]:
+        create_user(user)
+        save_db()
+    if json_data["users"][user]["bank"] < num:
+        return f"You do not have ${num} on your bank account."
+    get_money(user, num)
+    json_data["users"][user]["bank"] -= num
+    save_db()
+    return f"You withdrew ${num} from your bank account."
 
 ##################### BASIC ACTIONS #####################
 
@@ -140,6 +178,7 @@ def do_dig(user):
         items = "Nothing.\n"
     if shovel and random.randint(1, 100) == 1:
             items += "But you broke your shovel !"
+            del_items("shovel", 1)
     save_db()
     return f"""##### YOU DIGGED #####
 {items}"""
@@ -203,3 +242,206 @@ def do_shop_see(user):
         items = "There is nothing in the shop."
     return f"""##### SHOP #####
 {items}"""
+
+def do_dive(user):
+    if not user in json_data["users"]:
+        create_user(user)
+    proba = json_data["users"][user]["probas"]["dive"]
+    items = ""
+    if has_item(user, "wetsuit", 1):
+        wetsuit = 1
+    else:
+        wetsuit = 0
+    for k, i in proba.items():
+        r = random.randint(0, 100)
+        if not wetsuit:
+            i = i // 2
+        if r < i:
+            add_items(user, k, 1)
+            items += f"- {k} x{1}\n"
+    if items == "":
+        items = "Nothing.\n"
+    if wetsuit and random.randint(1, 100) == 1:
+            items += "But you broke your wetsuit !"
+            del_items("wetsuit", 1)
+    save_db()
+    return f"""##### YOU FOUND #####
+{items}"""
+
+##################### MARKET #####################
+
+def update_marketprice():
+    if len(json_data["market"]) < 50 and json_data["marketprice"] > 1:
+        if random.randint(1, 30): json_data["marketprice"] -= 1
+        if random.randint(1, 30): json_data["inflation"] += 1
+    elif json_data["marketprice"] < 100:
+        if random.randint(1, 30): json_data["marketprice"] += 1
+        if random.randint(1, 30): json_data["inflation"] -= 1
+
+def do_market_add(user, type, item, amount, for_item, for_amount):
+    type = type.lower()
+    item = item.lower()
+    for_item = for_item.lower()
+    if not user in json_data["users"]:
+        create_user(user)
+        save_db()
+    if not item in existing_items:
+        return f"{item} is not a valid item !"
+    if type != "buy" and type != "sell":
+        return f"""{type} is not a valid type in the market !
+type can be :
+buy : You want to buy something
+sell : You want to sell something"""
+    if type == "sell":
+        if not has_item(user, item, amount):
+            return f"You do not have {amount} {item} !"
+    else:
+        if not has_item(user, for_item, for_amount):
+            return f"You do not have {for_amount} {for_item} !"
+    if not for_item in existing_items and for_item != "coins":
+        return f"{for_item} is not a valid item !"
+    if for_item == "coins":
+        price = int(for_amount/100*json_data["marketprice"])
+    else:
+        if for_item in json_data["prices"]:
+            price = int((json_data["prices"][for_item]*for_amount)/100*json_data["marketprice"]*json_data["inflation"])
+        elif for_item in json_data["non_sellable_prices"]:
+            price = int((json_data["non_sellable_prices"][for_item]*for_amount)/100*json_data["marketprice"]*json_data["inflation"])
+        else:
+            return f"Item {for_item} has an unknown price !"
+        if price < 1:
+            price = 1
+        if not has_money(user, price):
+            return f"You need ${price} to post this offer !"
+        pay(user, price)
+    id = 0
+    while str(id) in json_data["market"]: id += 1
+    json_data["market"][str(id)] = {"user": user, "type": type, "item": item,
+    "amount": amount, "for_item": for_item, "for_amount": for_amount}
+    update_marketprice()
+    save_db()
+    return f"""##### MARKET #####
+You added an offer :
+Id : {id}
+Offer of type {type}.
+- {item} x{amount}
+- For : {for_item} x{for_amount}
+"""
+
+def do_market_remove(user, id):
+    id = str(id)
+    if not user in json_data["users"]:
+        create_user(user)
+        save_db()
+    if not id in json_data["market"]:
+        return f"id {id} does not exists !"
+    if user == json_data["market"][id]["user"]:
+        del json_data["market"][id]
+        update_marketprice()
+        save_db()
+        return f"You removed offer {id}."
+    return f"You connot remove offer {id}."
+
+def do_market_accept(user, id, amount):
+    id = str(id)
+    if not user in json_data["users"]:
+        create_user(user)
+        save_db()
+    if not id in json_data["market"]:
+        return f"id {id} does not exists !"
+    dest = json_data["market"][id]["user"]
+    if user == dest:
+        return "You cannot accept your own offer."
+    if amount == None: amount = 1
+    if json_data["market"][id]["type"] == "sell":
+        if amount > json_data["market"][id]["amount"]:
+            return "You cannot buy so much at this offer."
+        if json_data["market"][id]["for_item"] == "coins":
+            if not has_money(user, amount):
+                for_amount = json_data["market"][id]["for_amount"]
+                return f"You do not have ${for_amount} !"
+            add_items(user, json_data["market"][id]["item"], amount)
+            del_items(dest, json_data["market"][id]["item"], amount)
+            price = int(json_data["market"][id]["for_amount"]/json_data["market"][id]["amount"]*amount)
+            pay(user, price)
+            get_money(dest, price)
+        else:
+            if not has_item(user, json_data["market"][id]["for_item"], amount):
+                for_item = json_data["market"][id]["for_item"]
+                return f"You do not have {amount} {for_item} !"
+            dest = json_data["market"][id]["user"]
+            add_items(user, json_data["market"][id]["item"], amount)
+            del_items(dest, json_data["market"][id]["item"], amount)
+            add_items(dest, json_data["market"][id]["for_item"], amount)
+            del_items(user, json_data["market"][id]["for_item"], amount)
+    elif json_data["market"][id]["type"] == "buy":
+        dest = json_data["market"][id]["user"]
+        if amount > json_data["market"][id]["for_amount"]:
+            return "You cannot sell so much at this offer."
+        if json_data["market"][id]["for_item"] == "coins":
+            if not has_money(dest, amount):
+                coins = json_data["market"][id]["for_amount"]
+                return f"You do not have ${coins} !"
+            del_items(user, json_data["market"][id]["item"], amount)
+            add_items(dest, json_data["market"][id]["item"], amount)
+            price = int(json_data["market"][id]["for_amount"]/json_data["market"][id]["amount"]*amount)
+            get_money(user, price)
+            pay(dest, price)
+        else:
+            if not has_item(user, json_data["market"][id]["item"], amount):
+                for_item = json_data["market"][id]["for_item"]
+                return f"You do not have {amount} {for_item} !"
+            del_items(user, json_data["market"][id]["item"], amount)
+            add_items(dest, json_data["market"][id]["item"], amount)
+            del_items(dest, json_data["market"][id]["for_item"], amount)
+            add_items(user, json_data["market"][id]["for_item"], amount)
+    else:
+        type = json_data["market"][id]["type"]
+        return f"Unknown offer type {type}"
+    if json_data["market"][id]["type"] == "buy":
+        text = f"""You accepted offer {id}
+You got :
+{json_data["market"][id]["for_item"]} x{amount}"""
+    else:
+        text = f"""You accepted offer {id}
+You got :
+{json_data["market"][id]["item"]} x{amount}"""
+    del json_data["market"][id]
+    update_marketprice()
+    save_db()
+    return text
+
+def do_market_see(user, item):
+    item = item.lower()
+    if not item in existing_items:
+        return f"{item} is not a valid item !"
+    if not user in json_data["users"]:
+        create_user(user)
+        save_db()
+    out = ""
+    for i in json_data["market"]:
+        if json_data["market"][i]["item"] == item:
+            out += f"""MARKET OFFER
+Id : {i}
+Offer of type {json_data["market"][i]["type"]}.
+- {json_data["market"][i]["item"]} x{json_data["market"][i]["amount"]}
+- For : {json_data["market"][i]["for_item"]} x{json_data["market"][i]["for_amount"]}
+"""
+        if json_data["market"][i]["item"] == user:
+            out += "(Posted by you)\n"
+    if out == "":
+        return "Ow, there is nothing in the market for this item ..."
+    else:
+        return f"##### MARKET #####\n\n{out}"
+
+##################### ECONOMY #####################
+
+def view_stat(user):
+    if not user in json_data["users"]:
+        create_user(user)
+        save_db()
+    return f"""##### ECONOMY #####
+Inflation : {json_data["inflation"]}%
+Offer price in the market : {json_data["marketprice"]}%
+of the income of the offer.
+"""
--- main.py
+++ main.py
@@ -5,6 +5,25 @@ bot = interactions.Client(token="token")
 
 calls = {}
 
+chiptune_links = [
+    "https://www.youtube.com/watch?v=Hj3W6nthrKU"
+]
+
+@bot.command(
+    name="chiptune",
+    description="Get a link to some cool chiptune.",
+    default_scope=False
+)
+
+async def chiptune(ctx: interactions.CommandContext):
+    user = f"{ctx.user.id}"
+    if not user in json_data["users"]:
+        create_user(user)
+        save_db()
+    out = f"""Some chiptune :
+{random.choice(chiptune_links)}"""
+    await ctx.send(f"```{out}```")
+
 ##################### PROFILE #####################
 
 @bot.command(
@@ -110,6 +129,177 @@ async def shop_buy(ctx: interactions.CommandContext, item: str, amount: int = 1)
 async def shop_view(ctx: interactions.CommandContext):
     user = f"{ctx.user.id}"
     out = do_shop_see(user)
+    await ctx.send(f"```{out}```")
+
+@bot.command(
+    name="dive",
+    description="Dive in the sea to find some items.",
+    default_scope=False
+)
+
+async def dive(ctx: interactions.CommandContext):
+    user = f"{ctx.user.id}"
+    out = do_dive(user)
+    await ctx.send(f"```{out}```")
+
+@bot.command(
+    name="deposit",
+    description="Deposit some coins on your bank account.",
+    options = [
+        interactions.Option(
+            name="num",
+            description="How many coins do you want to deposit.",
+            type=interactions.OptionType.INTEGER,
+            required=True,
+        )
+    ],
+    default_scope=False
+)
+
+async def deposit(ctx: interactions.CommandContext, num: int):
+    user = f"{ctx.user.id}"
+    out = do_deposit(user, num)
+    await ctx.send(f"```{out}```")
+
+@bot.command(
+    name="withdraw",
+    description="withdraw some coins from your bank account.",
+    options = [
+        interactions.Option(
+            name="num",
+            description="How many coins do you want to withdraw.",
+            type=interactions.OptionType.INTEGER,
+            required=True,
+        )
+    ],
+    default_scope=False
+)
+
+async def withdraw(ctx: interactions.CommandContext, num: int):
+    user = f"{ctx.user.id}"
+    out = do_withdraw(user, num)
+    await ctx.send(f"```{out}```")
+
+##################### MARKET #####################
+
+@bot.command(
+    name="market_add",
+    description="Sell or buy items to get some coins/items.",
+    options = [
+        interactions.Option(
+            name="type",
+            description="The type of your post 'sell' or 'buy'.",
+            type=interactions.OptionType.STRING,
+            required=True
+        ),
+        interactions.Option(
+            name="item",
+            description="Which item you want to buy/sell",
+            type=interactions.OptionType.STRING,
+            required=True
+        ),
+        interactions.Option(
+            name="amount",
+            description="How many items you want to buy/sell",
+            type=interactions.OptionType.INTEGER,
+            required=True
+        ),
+        interactions.Option(
+            name="for_item",
+            description="For which item ? Set this arg. to 'coins' if you want to get coins.",
+            type=interactions.OptionType.STRING,
+            required=True
+        ),
+        interactions.Option(
+            name="for_amount",
+            description="For how many items (or coins) ?",
+            type=interactions.OptionType.INTEGER,
+            required=True
+        )
+    ],
+    default_scope=False
+)
+
+async def market_add(ctx: interactions.CommandContext, type: str,
+item: str, for_item: str, amount: int = 1, for_amount: int = 1):
+    user = f"{ctx.user.id}"
+    out = do_market_add(user, type, item, amount, for_item, for_amount)
+    await ctx.send(f"```{out}```")
+
+@bot.command(
+    name="market_remove",
+    description="Remove an offer from the market.",
+    options = [
+        interactions.Option(
+            name="id",
+            description="Id of the offer.",
+            type=interactions.OptionType.INTEGER,
+            required=True
+        )
+    ],
+    default_scope=False
+)
+
+async def market_remove(ctx: interactions.CommandContext, id: int):
+    user = f"{ctx.user.id}"
+    out = do_market_remove(user, id)
+    await ctx.send(f"```{out}```")
+
+@bot.command(
+    name="market_accept",
+    description="Accept an offer from the market.",
+    options = [
+        interactions.Option(
+            name="id",
+            description="Id of the offer.",
+            type=interactions.OptionType.INTEGER,
+            required=True
+        ),
+        interactions.Option(
+            name="amount",
+            description="How many items do you want to buy/sell.",
+            type=interactions.OptionType.INTEGER,
+            required=False
+        )
+    ],
+    default_scope=False
+)
+
+async def market_accept(ctx: interactions.CommandContext, id: int, amount: int = None):
+    user = f"{ctx.user.id}"
+    out = do_market_accept(user, id, amount)
+    await ctx.send(f"```{out}```")
+
+@bot.command(
+    name="market_view",
+    description="Search an offer in the market.",
+    options = [
+        interactions.Option(
+            name="item",
+            description="The item that you want to buy/sell.",
+            type=interactions.OptionType.STRING,
+            required=True
+        )
+    ],
+    default_scope=False
+)
+
+async def market_view(ctx: interactions.CommandContext, item: str):
+    user = f"{ctx.user.id}"
+    out = do_market_see(user, item)
+    await ctx.send(f"```{out}```")
+
+##################### ECONOMY #####################
+
+@bot.command(
+    name="economy_info",
+    description="Economy infos.",
+    default_scope=False
+)
+
+async def economy_info(ctx: interactions.CommandContext):
+    user = f"{ctx.user.id}"
+    out = view_stat(user)
     await ctx.send(f"```{out}```")
 
 bot.start()
